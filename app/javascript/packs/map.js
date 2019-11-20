39b7b3dc7f879740a9a61bd97b2cc5045cb2d48a
leonard percival <leonardpercival@gmail.com>
2019-11-20 10:29:03 +0000

    lecture finished

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,32 +1,42 @@
 import 'mapbox-gl/dist/mapbox-gl.css';
-import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
 
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
-  // const fitMapToMarkers = (map, markers) => {
-  //   const bounds = new mapboxgl.LngLatBounds();
-  //   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  //   map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
-  // };
+  const fitMapToMarkers = (map, markers) => {
+    const bounds = new mapboxgl.LngLatBounds();
+    markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 5000 });
+  };
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = process.env.MAPBOX_API_KEY;
     // mapboxgl.accessToken = mapElement.dataset.mapbox-api-key
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/leonardpercival/ck374vw1701mv1cpjc2tc64ig'
     });
 
-    // const markers = JSON.parse(mapElement.dataset.markers);
-    //   markers.forEach((marker) => {
-    //     new mapboxgl.Marker()
-    //       .setLngLat([ marker.lng, marker.lat ])
-    //       .addTo(map);
-    //   });
+    const markers = JSON.parse(mapElement.dataset.markers);
+    markers.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
+
+    new mapboxgl.Marker()
+      .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup) // add this
+      .addTo(map);
+    });
+
+    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+                                      mapboxgl: mapboxgl }));
+
+    fitMapToMarkers(map, markers);
   }
 
 };
